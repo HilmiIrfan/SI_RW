2349cae25eaa58bf00af6fbeecb4af8dd8ff4e02
= <113957634+febbymathelda@users.noreply.github.com>
2024-06-04 00:31:53 +0700

    Update TAMPILAN Galeri warga

--- resources/views/galeri/galeri.blade.php
+++ resources/views/galeri/galeri.blade.php
@@ -1,165 +1,156 @@
+
 @extends('layout.template')
 
 @section('content')
-    
-    <style>
-        body {
-            font-family: Ubuntu;
-            margin: 10;
-            padding: 10;
-            background-color: #f0f0f0;
-        }
 
-        h1 {
-            text-align: center;
-            margin-top: 25px;
-        }
+<link rel="stylesheet" href="{{asset ('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
 
-        .gallery-container {
-            display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
-            gap: 10px;
-            padding: 10px;
-            max-width: 1000px;
-            margin: 0 auto;
-        }
+<link rel="stylesheet" href="{{asset ('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
 
-        .gallery-item {
-            border: 2px solid #ccc;
-            overflow: hidden;
-            position: relative;
-        }
+<link rel="stylesheet" href="{{asset ('adminlte/plugins/ekko-lightbox/ekko-lightbox.css') }}">
 
-        .gallery-item img {
-            display: block;
-            max-width: 250px; 
-            max-height: 250px; 
-            width: auto;
-            height: auto;
-            transition: transform 0.3s ease;
-        }
+<link rel="stylesheet" href="{{asset ('adminlte/dist/css/adminlte.min.css?v=3.2.0') }}">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
 
-        .gallery-item:hover img {
-            transform: scale(1.1);
-        }
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+</li>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+<a href="#" class="dropdown-item">
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="fullscreen" href="#" role="button">
+<i class="fas fa-expand-arrows-alt"></i>
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+<i class="fas fa-th-large"></i>
+</a>
+</li>
+</ul>
+</nav>
+<div class="container-fluid">
+<div class="row">
+<div class="col-12">
+<div class="card card-primary">
+<div class="card-header">
+<h4 class="card-title">Galeri RW 01 Kesatrian Dalam</h4>
+</div>
+<div class="card-body">
+<div>
 
-        .fasilitas-image .description {
-            padding: 5px; /* Jarak yang lebih besar */
-            width: 150px; /* Lebar yang lebih besar */
-        }
-        .tanamankeluarga-image .description {
-            padding: 5px; /* Jarak yang lebih besar */
-            width: 150px; /* Lebar yang lebih besar */
-        }
-        .lapanganvoli-image .description {
-            padding: 5px; /* Jarak yang lebih kecil */
-            width: 150px; /* Lebar yang lebih kecil */
-        }
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f0f0f0;
-        }
+</div>
+<div>
+<div class="filter-container p-0 row">
+<div class="filtr-item col-sm-2" data-category="1" data-sort="Fasilitas sample">
+<a href="{{ asset('adminlte/dist/img/Fasilitas.jpg') }}" data-toggle="lightbox" data-title="sample 1 - Fasilitas">
+<img src="{{ asset('adminlte/dist/img/Fasilitas.jpg') }}" class="img-fluid mb-2" alt="Fasilitas sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="2, 4" data-sort="Fasilitas sample">
+<a href="{{ asset('adminlte/dist/img/poskaming.jpg') }}" data-toggle="lightbox" data-title="Fasilitas - black">
+<img src="{{ asset('adminlte/dist/img/poskaming.jpg') }}" class="img-fluid mb-2" alt="Fasilitas sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="3, 4" data-sort="red sample">
+<a href="{{ asset('adminlte/dist/img/lomba.jpg') }}" data-toggle="lightbox" data-title="sample 3 - red">
+<img src="{{ asset('adminlte/dist/img/lomba.jpg') }}" class="img-fluid mb-2" alt="red sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="3, 4" data-sort="red sample">
+<a href="{{ asset('adminlte/dist/img/jalansehat.jpg') }}" data-toggle="lightbox" data-title="sample 4 - red">
+<img src="{{ asset('adminlte/dist/img/jalansehat.jpg') }}" class="img-fluid mb-2" alt="red sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
+<a href="{{ asset('adminlte/dist/img/tanamankeluarga.jpg') }}" data-toggle="lightbox" data-title="sample 5 - black">
+<img src="{{ asset('adminlte/dist/img/tanamankeluarga.jpg') }}" class="img-fluid mb-2" alt="black sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
+<a href="{{ asset('adminlte/dist/img/lapanganVoli.jpg') }}" data-toggle="lightbox" data-title="sample 6 - white">
+<img src="{{ asset('adminlte/dist/img/lapanganVoli.jpg') }}" class="img-fluid mb-2" alt="white sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
+<a href="{{ asset('adminlte/dist/img/jalanKesatrian.png') }}" data-toggle="lightbox" data-title="sample 7 - white">
+<img src="{{ asset('adminlte/dist/img/jalanKesatrian.png') }}" class="img-fluid mb-2" alt="white sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
+<a href="{{ asset('adminlte/dist/img/bansos.jpeg') }}" data-toggle="lightbox" data-title="sample 8 - black">
+<img src="{{ asset('adminlte/dist/img/bansos.jpeg') }}" class="img-fluid mb-2" alt="black sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="3, 4" data-sort="white sample">
+<a href="{{ asset('adminlte/dist/img/bapakrw.jpg') }}" data-toggle="lightbox" data-title="sample 9 - white">
+<img src="{{ asset('adminlte/dist/img/bapakrw.jpg') }}" class="img-fluid mb-2" alt="white sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
+<a href="{{ asset('adminlte/dist/img/acara.jpg') }}" data-toggle="lightbox" data-title="sample 10 - white">
+<img src="{{ asset('adminlte/dist/img/acara.jpg') }}" class="img-fluid mb-2" alt="white sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
+<a href="{{ asset('adminlte/dist/img/lomba.jpg') }}" data-toggle="lightbox" data-title="sample 11 - white">
+<img src="{{ asset('adminlte/dist/img/lomba.jpg') }}" class="img-fluid mb-2" alt="white sample" />
+</a>
+</div>
+<div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
+<a href="{{ asset('adminlte/dist/img/lomba.jpg') }}" data-toggle="lightbox" data-title="sample 12 - black">
+<img src="{{ asset('adminlte/dist/img/lomba.jpg') }}" class="img-fluid mb-2" alt="black sample" />
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
 
-        .navbar {
-            display: flex;
-            align-items: center;
-            background-color: #e0e0e0;
-            padding: 10px;
-            justify-content: space-between;
-        }
+</div>
 
-        .navbar img {
-            height: 30px; /* Atur tinggi logo sesuai keinginan Anda */
-            width: auto;/* Lebar otomatis agar proporsinya tetap terjaga */
-            
-        }
 
-        .navbar .menu {
-            display: flex;
-            flex-grow: 1;
-            justify-content: space-around;
-            margin-left: 20px;
-            margin-right: 20px;
-        }
 
-        .navbar .menu a {
-            text-decoration: none;
-            color: #000;
-            padding: 10px 15px;
-            border-radius: 4px;
-            transition: background-color 0.3s ease;
-        }
+<aside class="control-sidebar control-sidebar-dark">
 
-        .navbar .menu a:hover,
-        .navbar .menu .active {
-            background-color: #5A9BD5;
-            color: #fff;
-        }
+</aside>
 
-        .navbar .search {
-            background-color: #5A9BD5;
-            padding: 5px 15px;
-            border-radius: 4px;
-            color: #fff;
-        }
+</div>
 
-        
-    </style>
-    
-    <br>
-    <div class="gallery-container">
-        <div class="gallery-item fasilitas-image">
-            <img src="{{ asset('adminlte/dist/img/Fasilitas.jpg') }}" alt="Fasilitas">
-            <div class="description">
-                <p><b>Fasilitas</b></p>
-                <p>Posyandu</p>
-            </div>
-        </div>
-        <div class="gallery-item tanamankeluarga-image">
-            <img src="{{ asset('adminlte/dist/img/tanamankeluarga.jpg') }}" alt="tanamankeluarga">
-            <div class="description">
-                <p><b>Fasilitas</b></p>
-                <p>Tanaman Obat Keluarga</p>
-            </div>
-        </div>
-        <div class="gallery-item lapanganvoli-image">
-            <img src="{{ asset('adminlte/dist/img/lapanganVoli.jpg') }}" alt="lapanganVoli">
-            <div class="description">
-                <p><b>Fasilitas</b></p>
-                <p>Lapangan Voli Bersama</p>
-            </div>
-        </div>
-        <div class="gallery-item">
-            <img src="{{ asset('adminlte/dist/img/jalansehat.jpg') }}" alt="jalansehat">
-            <div class="description">
-                <p><b>Kegiatan</b></p>
-                <p>Jalan Sehat Agustus 2023</p>
-            </div>
-        </div>
-        <div class="gallery-item">
-            <img src="{{ asset('adminlte/dist/img/poskaming.jpg') }}" alt="poskaming">
-            <div class="description">
-                <p><b>Fasilitas</b></p>
-                <p>Pos Kamling</p>
-            </div>
-        </div>
-        <div class="gallery-item">
-            <img src="{{ asset('adminlte/dist/img/kerjaBakti.jpg') }}" alt="kerjaBakti">
-            <div class="description">
-                <p><b>Kegiatan</b></p>
-                <p>Kerja Bakti</p>
-            </div>
-        </div>
-        <div class="gallery-item">
-            <img src="{{ asset('adminlte/dist/img/lomba.jpg') }}" alt="lomba">
-            <div class="description">
-                <p><b>Kegiatan</b></p>
-                <p>Lomba 17 Agustus 2022</p>
-            </div>
-        </div>
-        
-    </div>
 
+<script src="../plugins/jquery/jquery.min.js"></script>
+
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="../plugins/ekko-lightbox/ekko-lightbox.min.js"></script>
+
+<script src="../dist/js/adminlte.min.js?v=3.2.0"></script>
+
+<script src="../plugins/filterizr/jquery.filterizr.min.js"></script>
+
+<script src="../dist/js/demo.js"></script>
+
+<script>
+  $(function () {
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+      event.preventDefault();
+      $(this).ekkoLightbox({
+        alwaysShowClose: true
+      });
+    });
+
+    $('.filter-container').filterizr({gutterPixels: 3});
+    $('.btn[data-filter]').on('click', function() {
+      $('.btn[data-filter]').removeClass('active');
+      $(this).addClass('active');
+    });
+  })
+</script>
+</body>
+</html>
 @endsection
